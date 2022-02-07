7d69fb44d8ef4da7669035f4f3222880fca1f994
Hermann Rösch <mr.guty@Hotmail.com>
2022-02-07 05:48:21 -0500

    Stage 4 - added "Function" oriented refactoring

--- University Admission Procedure/stage4/university_stage4.go
+++ University Admission Procedure/stage4/university_stage4.go
@@ -24,7 +24,7 @@ var orderedDepartments = []string{
 	"Physics",
 }
 
-type FinalApplicant struct {
+type Applicant struct {
 	fullName string
 	score    float64
 }
@@ -50,6 +50,46 @@ func readApplicantPreferences(file *os.File) []ApplicantPreferences {
 	return a
 }
 
+func chooseFaculty(applicants []ApplicantPreferences, departments map[string][]Applicant, nApplicants int) map[string][]Applicant {
+	var used []string
+
+	for i := 0; i < 3; i++ {
+		for _, dep := range orderedDepartments {
+			applicantsSorted := sortByMajorScore(applicants)
+			for _, applicant := range applicantsSorted {
+				if applicant.departments[i] == dep && len(departments[dep]) < nApplicants && !contains(used, applicant.fullName) {
+					score := applicant.score
+
+					departments[dep] = append(departments[dep], Applicant{applicant.fullName, score})
+
+					used = append(used, applicant.fullName)
+				}
+			}
+		}
+	}
+
+	for _, v := range departments {
+		sort.Slice(v, func(i, j int) bool {
+			if v[i].score != v[j].score {
+				return v[i].score > v[j].score
+			}
+			return v[i].fullName < v[j].fullName
+		})
+	}
+	return departments
+}
+
+func sortByMajorScore(a []ApplicantPreferences) []ApplicantPreferences {
+	sort.Slice(a, func(i, j int) bool {
+		first, second := a[i], a[j]
+		if first.score != second.score {
+			return first.score > second.score
+		}
+		return first.fullName < second.fullName
+	})
+	return a
+}
+
 func contains(s []string, e string) bool {
 	for _, a := range s {
 		if a == e {
@@ -59,19 +99,39 @@ func contains(s []string, e string) bool {
 	return false
 }
 
-func sortByDept(a []ApplicantPreferences) []ApplicantPreferences {
-	sort.Slice(a, func(i, j int) bool {
-		if a[i].score != a[j].score {
-			return a[i].score > a[j].score
+func prepareFinalOrder(departments map[string][]Applicant) {
+	for _, dep := range orderedDepartments {
+		sort.Slice(departments[dep], func(i, j int) bool {
+			if departments[dep][i].score != departments[dep][j].score {
+				return departments[dep][i].score > departments[dep][j].score
+			}
+			return departments[dep][i].fullName < departments[dep][j].fullName
+		})
+	}
+}
+
+func showAccepted(departments map[string][]Applicant) {
+	for _, dep := range orderedDepartments {
+		fmt.Println(dep)
+		for _, v := range departments[dep] {
+			fmt.Printf("%s %.2f\n", v.fullName, v.score)
 		}
-		return a[i].fullName < a[j].fullName
-	})
-	return a
+		fmt.Println()
+	}
 }
 
 func main() {
 	var nApplicants int
 	fmt.Scanln(&nApplicants)
+
+	// the 'departments' map would be equal to the 'final' map from the 'refactored' branch solution!
+	departments := map[string][]Applicant{
+		"Biotech":     {},
+		"Chemistry":   {},
+		"Engineering": {},
+		"Mathematics": {},
+		"Physics":     {},
+	}
 
 	file, err := os.Open("./applicant_list_4.txt")
 	if err != nil {
@@ -81,45 +141,9 @@ func main() {
 
 	applicants := readApplicantPreferences(file)
 
-	departments := map[string][]FinalApplicant{
-		"Biotech":     {},
-		"Chemistry":   {},
-		"Engineering": {},
-		"Mathematics": {},
-		"Physics":     {},
-	}
+	departments = chooseFaculty(applicants, departments, nApplicants)
 
-	var used []string
+	prepareFinalOrder(departments)
 
-	for i := 0; i < 3; i++ {
-		for _, dep := range orderedDepartments {
-			applicantsSorted := sortByDept(applicants)
-			for _, applicant := range applicantsSorted {
-				if applicant.departments[i] == dep && len(departments[dep]) < nApplicants && !contains(used, applicant.fullName) {
-					score := applicant.score
-
-					departments[dep] = append(departments[dep], FinalApplicant{applicant.fullName, score})
-
-					used = append(used, applicant.fullName)
-				}
-			}
-		}
-	}
-
-	for _, dep := range orderedDepartments {
-		sort.Slice(departments[dep], func(i, j int) bool {
-			if departments[dep][i].score != departments[dep][j].score {
-				return departments[dep][i].score > departments[dep][j].score
-			}
-			return departments[dep][i].fullName < departments[dep][j].fullName
-		})
-	}
-
-	for _, dep := range orderedDepartments {
-		fmt.Println(dep)
-		for _, v := range departments[dep] {
-			fmt.Printf("%s %.2f\n", v.fullName, v.score)
-		}
-		fmt.Println()
-	}
+	showAccepted(departments)
 }
